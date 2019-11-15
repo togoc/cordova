5d125f4302dbc73f4d4b7acf1ade8f4b4976b269
togoc <49116563+togoc@users.noreply.github.com>
2019-11-15 18:30:49 +0800

    update

--- demo2/www/js/index.js
+++ demo2/www/js/index.js
@@ -1,51 +1,27 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-    },
+let b1 = document.querySelector('.b1')
+let b2 = document.querySelector('.b2')
+let img = document.querySelector('img')
 
-    // deviceready Event Handler
-    //
-    // Bind any cordova events here. Common events are:
-    // 'pause', 'resume', etc.
-    onDeviceReady: function() {
-        this.receivedEvent('deviceready');
-    },
 
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
+document.addEventListener("deviceready", function() {
+    console.log("deviceready is done")
+    b1.addEventListener("click", takePic, false)
+    b2.addEventListener("click", getPic, false)
+}, false)
 
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
+function takePic() {
+    navigator.camera.getPicture(success, error, {})
 
-        console.log('Received Event: ' + id);
+    function success(url) {
+        console.log("拍照成功")
+        img.src = url
     }
-};
 
-app.initialize();
+    function error() {
+        console.log("拍照失败")
+    }
+}
 
+function getPic() {
 
-document.addEventListener("deviceready", () => {
-    console.log("ok")
-})
+}
